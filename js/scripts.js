6913a2a2fee8e806e8a8ee8089adcd9fc83e3acb
Torin Huff <torinhuff@yahoo.com>
2018-10-23 21:30:22 -0700

    this will display the total price when a user clicks the 'order' button and the 'submit' button

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,66 @@
 //business-logic
+var globalPizza;
 
 function Pizza(userSize, userToppings) {
   this.size = userSize;
   this.toppings = userToppings;
 }
 
-Pizza.prototype.alertSize = function() {
-  if (this.size == "personal") {
-    alert("Personal Pizza")
-  } else if (this.size == "small") {
-    alert("Small Pizza")
-  } else if (this.size == "medium") {
-    alert("Medium Pizza")
-  } else if (this.size == "large") {
-    alert("Large Pizza")
-  } else if (this.size == "family") {
-    alert("Family Pizza")
-  }
-}
-
-Pizza.prototype.alertToppings = function(){
+Pizza.prototype.toppingsList = function(){
   var toppingsList = "";
   for(var i = 0; i < this.toppings.length; i++) {
     toppingsList = toppingsList + this.toppings[i];
   }
-  alert(toppingsList);
+  return toppingsList;
 }
 
+Pizza.prototype.finalPrice = function(){
+  var sizePrice = 0;
+  var toppingPrice = 3 * this.toppings.length;
+  if (this.size == "Personal Pizza") {
+    sizePrice = 7;
+  } else if (this.size == "Small Pizza") {
+    sizePrice = 9;
+  } else if (this.size == "Medium Pizza") {
+    sizePrice = 11;
+  } else if (this.size == "Large Pizza") {
+    sizePrice = 13;
+  } else if (this.size == "Family Pizza") {
+    sizePrice = 15;
+  }
+  var price = sizePrice + toppingPrice;
+  return price;
+}
 
+function updatePizza(){
+  var size = $("input[name='size']:checked").val();
+  var toppings = []; $("input[name='toppings']:checked").each(function(i,v){
+    toppings.push($(v).val());
+  });
 
+  if (globalPizza != null) {
+    globalPizza.size = size;
+    globalPizza.toppings = toppings;
+  } else {
+    globalPizza = new Pizza(size, toppings);
+  }
+}
+
+function makePizza(){
+  updatePizza();
+  $("p#final-price").text("$" + globalPizza.finalPrice());
+}
+
+function makeOrder() {
+  updatePizza();
+  $("p#user-total").text("$" + globalPizza.finalPrice());
+};
 
 
 //user-logic
 
-$(document).ready(function() {
-  $("form#pizza-size").submit(function(event) {
-    event.preventDefault();
-
-  });
-});
-
-function makePizza(){
-  var size = $("input[name='size']:checked").val();
-  var toppings = []; $("input[name='toppings']:checked").each(function(i,v){
-  toppings.push($(v).val());
-});
-  alert(toppings)
-  var newPizza = new Pizza(size, toppings);
-  newPizza.alertSize();
-  newPizza.alertToppings();
-}
+// $(document).ready(function() {
+//     event.preventDefault();
+//
+//   });
+// });
