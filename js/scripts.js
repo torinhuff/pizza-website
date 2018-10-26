1d147ac1b39eb074efc891eb9677246f11430757
Torin Huff <torinhuff@yahoo.com>
2018-10-26 12:21:17 -0700

    this will add delivery and pick up options, a form for delivery information, two new buttons, and more toppings.

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ Pizza.prototype.toppingsList = function(){
 
 Pizza.prototype.finalPrice = function(){
   var sizePrice = 0;
-  var toppingPrice = 3 * this.toppings.length;
+  var toppingPrice = 1 * this.toppings.length;
   if (this.size == "Personal Pizza") {
     sizePrice = 7;
   } else if (this.size == "Small Pizza") {
@@ -51,16 +51,59 @@ function makePizza(){
   $("p#final-price").text("$" + globalPizza.finalPrice());
 }
 
-function makeOrder() {
+function checkOrder() {
   updatePizza();
   $("p#user-total").text("$" + globalPizza.finalPrice());
 };
 
+function deliverOrder() {
+  $(".delivery-info").show();
+  $(".pick-up-message").hide();
+}
+
+function pickUp() {
+  $(".pick-up-message").show();
+  $(".delivery-info").hide();
+}
+
+function Contact(first, last, street, city, state) {
+  this.firstName = first;
+  this.lastName = last;
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
 
 //user-logic
 
-// $(document).ready(function() {
-//     event.preventDefault();
-//
-//   });
-// });
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedStreet = $("input#new-street").val();
+    var inputtedCity = $("input#new-city").val();
+    var inputtedState = $("input#new-state").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedStreet, inputtedCity, inputtedState);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+      $(".street").text(newContact.street);
+      $(".city").text(newContact.city);
+      $(".state").text(newContact.state);
+    });
+
+  });
+});
